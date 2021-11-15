811dfacd05c22ce69339736496f0decc9e1ae6b8
Abiodun Segun <84022331+realshegz-github@users.noreply.github.com>
2021-11-15 17:35:48 +0100

    making the navbar static

---
+++ myScript.js
@@ -0,0 +1,13 @@
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add('sticky');
+	} else {
+		navbar.classList.remove('sticky');
+	}
+}
+window.onscroll = function () {
+	myFunction();
+};
